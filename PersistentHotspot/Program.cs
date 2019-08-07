14374edef70a4a6b36b552cfa321abfb9e5ca543
ashvin.bhuttoo <ashvin.bhuttoo@gmail.com>
2019-08-07 21:40:48 +0400

    handle potential issue with usb wifi adapter disconnected

--- PersistentHotspot/Program.cs
+++ PersistentHotspot/Program.cs
@@ -29,47 +29,55 @@ namespace PersistentHotspot
 
             while (true)
             {
-                var connectionProfile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
-                var tetheringManager = Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
-                if (tetheringManager.TetheringOperationalState == Windows.Networking.NetworkOperators.TetheringOperationalState.On)
+                try
                 {
-                    //Console.WriteLine("Hotspot is on.");
-                    trayIcon.Text = "Hotspot is on.";
-                    trayIcon.Icon = green;
-                }
-                else
-                {
-                    //Console.Write("Hotspot is off, turning that shit on.. ");
-                    trayIcon.Text = "Turning on Hotspot...";
-                    trayIcon.Icon = amber;
-                    var result = (Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult) await tetheringManager.StartTetheringAsync();
-                    switch(result.Status)
+                    var connectionProfile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+                    var tetheringManager = Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager.CreateFromConnectionProfile(connectionProfile);
+                    if (tetheringManager.TetheringOperationalState == Windows.Networking.NetworkOperators.TetheringOperationalState.On)
                     {
-                        case Windows.Networking.NetworkOperators.TetheringOperationStatus.OperationInProgress:
-                            //Console.WriteLine("Operation In Progress..");
-                            trayIcon.Text = "Turning on Hotspot...";
-                            trayIcon.Icon = amber;
-                            break;
-                        case Windows.Networking.NetworkOperators.TetheringOperationStatus.Success:
-                            //Console.WriteLine("Success!");
-                            trayIcon.Text = "Hotspot is on.";
-                            trayIcon.Icon = green;
-                            break;
+                        //Console.WriteLine("Hotspot is on.");
+                        trayIcon.Text = "Hotspot is on.";
+                        trayIcon.Icon = green;
+                    }
+                    else
+                    {
+                        //Console.Write("Hotspot is off, turning that shit on.. ");
+                        trayIcon.Text = "Turning on Hotspot...";
+                        trayIcon.Icon = amber;
+                        var result = (Windows.Networking.NetworkOperators.NetworkOperatorTetheringOperationResult)await tetheringManager.StartTetheringAsync();
+                        switch (result.Status)
+                        {
+                            case Windows.Networking.NetworkOperators.TetheringOperationStatus.OperationInProgress:
+                                //Console.WriteLine("Operation In Progress..");
+                                trayIcon.Text = "Turning on Hotspot...";
+                                trayIcon.Icon = amber;
+                                break;
+                            case Windows.Networking.NetworkOperators.TetheringOperationStatus.Success:
+                                //Console.WriteLine("Success!");
+                                trayIcon.Text = "Hotspot is on.";
+                                trayIcon.Icon = green;
+                                break;
 
-                        case Windows.Networking.NetworkOperators.TetheringOperationStatus.WiFiDeviceOff:
-                            //Console.WriteLine("Failure, WiFi Device Off!");
-                            trayIcon.Text = "Failure, WiFi Device Off!";
-                            trayIcon.Icon = red;
-                            break;
+                            case Windows.Networking.NetworkOperators.TetheringOperationStatus.WiFiDeviceOff:
+                                //Console.WriteLine("Failure, WiFi Device Off!");
+                                trayIcon.Text = "Failure, WiFi Device Off!";
+                                trayIcon.Icon = red;
+                                break;
 
-                        default:
-                        case Windows.Networking.NetworkOperators.TetheringOperationStatus.Unknown:
-                            //Console.WriteLine("Unknown Failure!");
-                            trayIcon.Text = "Unknown Failure!";
-                            trayIcon.Icon = red;
-                            break;
+                            default:
+                            case Windows.Networking.NetworkOperators.TetheringOperationStatus.Unknown:
+                                //Console.WriteLine("Unknown Failure!");
+                                trayIcon.Text = "Unknown Failure!";
+                                trayIcon.Icon = red;
+                                break;
+                        }
                     }
                 }
+                catch (Exception e) {
+                    trayIcon.Text = $"Error: {e.Message}";
+                    trayIcon.Icon = red;
+                };
+                
                 System.Threading.Thread.Sleep(10000); //wait 10s
             }           
         }
